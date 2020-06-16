4b7d82d35db4627671bd2b1f60eaa7c51ece2f62
solaryasha <solaryasha@gmail.com>
2020-06-16 12:55:35 +0300

    My first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('hello mate academy!');
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+const rows = tbody.querySelectorAll('tr');
+
+thead.addEventListener('click', e => {
+  const index = e.target.cellIndex;
+
+  const sortedRows = [ ...rows ].sort((a, b) => {
+    const firstElem = a.cells[index].textContent;
+    const secondElem = b.cells[index].textContent;
+
+    return firstElem.localeCompare(secondElem);
+  });
+
+  tbody.append(...sortedRows);
+});
